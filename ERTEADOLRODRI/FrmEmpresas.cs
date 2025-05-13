3311d719cdb860c53c51485229765f8e19718cfc
RodriGitTS <127401631+RodriGitTS@users.noreply.github.com>
2025-05-13 10:43:59 +0200

    listar empresas y formulario añadir creados

--- ERTEADOLRODRI/FrmEmpresas.cs
+++ ERTEADOLRODRI/FrmEmpresas.cs
@@ -16,5 +16,40 @@ namespace ERTEADOLRODRI
         {
             InitializeComponent();
         }
+
+        private void FrmEmpresas_Load(object sender, EventArgs e)
+        {
+            using (bd_ertesEntities objBD = new bd_ertesEntities())
+            {
+
+                var listaEmpresas = from emp in objBD.EMPRESAS
+                                    join s in objBD.SECTORES on emp.Sector
+                                    equals s.Id_sector
+                                    orderby emp.Nombre
+                                    select new
+                                    {
+                                        Nombre_Empresa = emp.Nombre,
+                                        Nombre_Sector=s.Descripcion,
+                                        CIF = emp.Cif
+                                    };
+
+                dataGridEmpresas.DataSource = listaEmpresas.ToList();
+
+            }
+        }
+
+        private void dataGridEmpresas_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            FrmModAddEmpresas frmModEmpresa = new FrmModAddEmpresas();
+            frmModEmpresa.ShowDialog();
+        }
+
+        private void btnAnadir_Click(object sender, EventArgs e)
+        {
+
+            FrmModAddEmpresas frmModEmpresa = new FrmModAddEmpresas();
+            frmModEmpresa.ShowDialog();
+        }
+        
     }
 }
---
+++ ERTEADOLRODRI/FrmModAddEmpresas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ERTEADOLRODRI
+{
+    public partial class FrmModAddEmpresas : Form
+    {
+        public FrmModAddEmpresas()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmModAddEmpresas_Load(object sender, EventArgs e)
+        {
+            using (bd_ertesEntities objBD = new bd_ertesEntities())
+            {
+                var listaSectores = from s in objBD.SECTORES
+                                    orderby s.Descripcion
+                                    select new
+                                    {
+                                        Id_Sector = s.Id_sector,
+                                        Descripcion = s.Descripcion
+                                    };
+                comboBoxSector.DataSource = listaSectores.ToList();
+            }
+        }
+    }
+}
