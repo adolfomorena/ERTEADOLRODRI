fad75bea5fe302de2c7a619d546294d52fb8c8e2
adolfomorena <adomorang@gamil.com>
2025-05-13 11:17:07 +0200

    diseño formulario

--- ERTEADOLRODRI/FrmEmpleados.cs
+++ ERTEADOLRODRI/FrmEmpleados.cs
@@ -16,5 +16,36 @@ namespace ERTEADOLRODRI
         {
             InitializeComponent();
         }
+
+        private void eMPLEADOSBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.eMPLEADOSBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.dsBD);
+
+        }
+
+        private void eMPLEADOSBindingNavigatorSaveItem_Click_1(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.eMPLEADOSBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.dsBD);
+
+        }
+
+        private void eMPLEADOSBindingNavigatorSaveItem_Click_2(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.eMPLEADOSBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.dsBD);
+
+        }
+
+        private void FrmEmpleados_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'dsBD.EMPLEADOS' Puede moverla o quitarla según sea necesario.
+            this.eMPLEADOSTableAdapter.Fill(this.dsBD.EMPLEADOS);
+
+        }
     }
 }
