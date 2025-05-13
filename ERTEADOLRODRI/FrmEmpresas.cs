60d09c9eea36facbff6af3991a95ee6f8cdd138e
RodriGitTS <127401631+RodriGitTS@users.noreply.github.com>
2025-05-13 09:46:45 +0200

    formulario principal creado y navegacion añadida

--- ERTEADOLRODRI/FrmEmpresas.cs
+++ ERTEADOLRODRI/FrmEmpresas.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ERTEADOLRODRI
 {
-    public partial class Form1 : Form
+    public partial class FrmEmpresas : Form
     {
-        public Form1()
+        public FrmEmpresas()
         {
             InitializeComponent();
         }
