4a7a97f93798c86ec430937ebf7774f10747bdcc
RodriGitTS <127401631+RodriGitTS@users.noreply.github.com>
2025-05-19 13:28:39 +0200

    CRUD para empresas añadido

--- ERTEADOLRODRI/FrmEmpresas.cs
+++ ERTEADOLRODRI/FrmEmpresas.cs
@@ -19,29 +19,14 @@ namespace ERTEADOLRODRI
 
         private void FrmEmpresas_Load(object sender, EventArgs e)
         {
-            using (bd_ertesEntities objBD = new bd_ertesEntities())
-            {
-
-                var listaEmpresas = from emp in objBD.EMPRESAS
-                                    join s in objBD.SECTORES on emp.Sector
-                                    equals s.Id_sector
-                                    orderby emp.Nombre
-                                    select new
-                                    {
-                                        Nombre_Empresa = emp.Nombre,
-                                        Nombre_Sector=s.Descripcion,
-                                        CIF = emp.Cif
-                                    };
-
-                dataGridEmpresas.DataSource = listaEmpresas.ToList();
-
-            }
+            CargarEmpresas();
         }
 
         private void dataGridEmpresas_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            FrmModAddEmpresas frmModEmpresa = new FrmModAddEmpresas();
-            frmModEmpresa.ShowDialog();
+            abrirFrmModEmpresa();
+
+            CargarEmpresas();
         }
 
         private void btnAnadir_Click(object sender, EventArgs e)
@@ -49,9 +34,62 @@ namespace ERTEADOLRODRI
 
             FrmModAddEmpresas frmModEmpresa = new FrmModAddEmpresas();
             frmModEmpresa.ShowDialog();
+
+            CargarEmpresas();
         }
 
         private void btnModificar_Click(object sender, EventArgs e)
+        {
+            abrirFrmModEmpresa();
+            CargarEmpresas();
+
+        }
+        public void CargarEmpresas()
+        {
+            using (bd_ertesEntities objBD = new bd_ertesEntities())
+            {
+                var listaEmpresas = from emp in objBD.EMPRESAS
+                                    join s in objBD.SECTORES on emp.Sector equals s.Id_sector
+                                    orderby emp.Nombre
+                                    select new
+                                    {
+                                        Nombre_Empresa = emp.Nombre,
+                                        Nombre_Sector = s.Descripcion,
+                                        CIF = emp.Cif
+                                    };
+
+                dataGridEmpresas.DataSource = listaEmpresas.ToList();
+            }
+        }
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (dataGridEmpresas.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("No ha seleccionado ninguna empresa", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            else
+            {
+                var empresa = dataGridEmpresas.SelectedRows[0].Cells["CIF"].Value.ToString();
+                using (bd_ertesEntities objBD = new bd_ertesEntities())
+                {
+                    var empresaSeleccionada = (from emp in objBD.EMPRESAS
+                                               where emp.Cif == empresa
+                                               select emp).FirstOrDefault();
+                    if (empresaSeleccionada == null)
+                    {
+                        MessageBox.Show("No se ha encontrado la empresa seleccionada", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+                    objBD.EMPRESAS.Remove(empresaSeleccionada);
+                    objBD.SaveChanges();
+                    MessageBox.Show("La empresa ha sido eliminada correctamente", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    CargarEmpresas();
+                }
+
+            }
+        }
+        public void abrirFrmModEmpresa()
         {
             if (dataGridEmpresas.SelectedRows.Count == 0)
             {
@@ -78,7 +116,8 @@ namespace ERTEADOLRODRI
                     );
                 frmModEmpresa.ShowDialog();
             }
-
         }
+
+       
     }
 }
--- ERTEADOLRODRI/FrmModAddEmpresas.cs
+++ ERTEADOLRODRI/FrmModAddEmpresas.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -64,7 +65,78 @@ namespace ERTEADOLRODRI
 
         private void btnAceptar_Click(object sender, EventArgs e)
         {
-            
+            using (bd_ertesEntities objBD=new bd_ertesEntities())
+            {
+
+                if(string.IsNullOrEmpty(textBoxDomicilio.Text)||
+                    string.IsNullOrEmpty(textBoxCIF.Text)||
+                        string.IsNullOrEmpty(textBoxNombre.Text)||
+                        string.IsNullOrEmpty(comboBoxSector.Text)){
+
+                    MessageBox.Show("Debe rellenar todos los campos", "Modificar empresa", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                if (EsCIF(textBoxCIF.Text))
+                {
+                    MessageBox.Show("CIF Invalido", "El CIF solo puede contener nunmeros", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                var empresaExistente = objBD.EMPRESAS
+                    .Where(x => x.Cif == textBoxCIF.Text).FirstOrDefault();
+
+                if (empresaExistente != null && empresaSeleccionada == null)
+                {
+                    MessageBox.Show("Ya existe una empresa con ese CIF", "Modificar empresa", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                if (empresaSeleccionada == null)
+                {
+                    var sectorSeleccionado = objBD.SECTORES
+                                           .Where(x => x.Descripcion == comboBoxSector.Text).
+                                           FirstOrDefault().Id_sector;
+
+                    EMPRESAS nuevaEmpresa = new EMPRESAS();
+                    nuevaEmpresa.Cif = textBoxCIF.Text;
+                    nuevaEmpresa.Nombre=textBoxNombre.Text;
+                    nuevaEmpresa.Domicilio = textBoxDomicilio.Text;
+                    nuevaEmpresa.Sector = sectorSeleccionado;
+
+                    objBD.EMPRESAS.Add(nuevaEmpresa);
+                    objBD.SaveChanges();
+
+                    MessageBox.Show("Empresa añadida correctamente", "Añadir empresa", 
+                        MessageBoxButtons.OK, 
+                        MessageBoxIcon.Information);
+
+                    this.Close();
+
+                }else
+                {
+                    var sectorSeleccionado = objBD.SECTORES
+                                           .Where(x => x.Descripcion == comboBoxSector.Text).
+                                           FirstOrDefault().Id_sector;
+                    empresaExistente = objBD.EMPRESAS.Where(x => x.Cif == textBoxCIF.Text).FirstOrDefault();
+                    empresaExistente.Nombre = textBoxNombre.Text;
+                    empresaExistente.Domicilio = textBoxDomicilio.Text;
+                    empresaExistente.Sector = sectorSeleccionado;
+
+                    objBD.SaveChanges();
+                    MessageBox.Show("Empresa modificada correctamente", "Modificar empresa",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+                    this.Close();
+                }
+
+            }
+               
         }
+        private bool EsCIF(string input)
+        {
+            return Regex.IsMatch(input, @"^[ABCDEFGHJKLMNPQRSUVW]\d{7}[0-9A-J]$");
+        }
+
     }
 }
