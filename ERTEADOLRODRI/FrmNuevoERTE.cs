5df895d3bb10a8fbf8c5a9f59e0525a83e56b2b6
adolfomorena <adomorang@gamil.com>
2025-05-27 09:31:51 +0200

    solución combobox empresas sin erte activo

--- ERTEADOLRODRI/FrmNuevoERTE.cs
+++ ERTEADOLRODRI/FrmNuevoERTE.cs
@@ -27,9 +27,11 @@ namespace ERTEADOLRODRI
             using (bd_ertesEntities objBD = new bd_ertesEntities())
             {
                 var qEmpresas = from emp in objBD.EMPRESAS
-                                where !(objBD.ERTES.Any(erte => erte.Empresa == emp.Cif && erte.Fecha_fin != null))
+                                where !emp.ERTES.Any(x => x.Fecha_fin == null)
                                 orderby emp.Nombre
                                 select emp;
+
+                cmbEmpresas.Items.Clear();
 
                 foreach (var emp in qEmpresas)
                 {
